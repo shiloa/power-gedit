88acfcb98dc052c3851b4deaba792499c774ea69
shiloa <shiloayalon@gmail.com>
2009-04-28 03:46:29 -0700

    removed some stuff to create folders - this will assume you have the ../gedit/pluggins and ../gedit/snippets folder already

--- install.sh
+++ install.sh
@@ -17,29 +17,10 @@ sudo cp themes/*.xml /usr/share/gtksourceview-2.0/styles
 sudo chmod 644 /usr/share/gtksourceview-2.0/styles/*.xml
 
 # install some fonts
-sudo apt-get install ttf-bitstream-vera ttf-dejavu ttf-dejavu ttf-dejavu-core ttf-dejavu-extra ttf-liberation ttf-mscorefonts-installer
+sudo apt-get install ttf-bitstream-vera ttf-dejavu ttf-dejavu ttf-dejavu-core ttf-dejavu-extra ttf-liberation
 
-if [ ! -d $HOME/.gnome2/gedit ] 
-then
-  mkdir -p ~/.gnome2/gedit
-  cp -r snippets ~/.gnome2/gedit/
-  sudo chmod 644 ~/.gnome2/gedit/snippets/*
-fi
-
-
-if [ ! -d $HOME/.gnome2/gedit/snippets ]
-then
-  mkdir -p ~/.gnome2/gedit/snippets
-fi
 cp snippets/* ~/.gnome2/gedit/snippets/
 sudo chmod 644 ~/.gnome2/gedit/snippets/*
 
-if [ ! -d $HOME/.gnome2/gedit/plugins ]
-then
-  mkdir -p ~/.gnome2/gedit/plugins
-fi
-
 cp -R plugins/* ~/.gnome2/gedit/plugins/
-sudo chmod +x -r ~/.gnome2/gedit/plugins/
-
-
+sudo chmod +x -R ~/.gnome2/gedit/plugins/
